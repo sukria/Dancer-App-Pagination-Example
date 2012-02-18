53b3064b66b92b7d51573494b45d822286482cfd
Hobbestigrou <hobbestigrou@erakis.im>
2012-02-18 15:40:14 +0100

    [Core] Paginationtest: Fix attribut param page.

--- lib/PaginationTest.pm
+++ lib/PaginationTest.pm
@@ -4,6 +4,12 @@ our $VERSION = '0.1';
 
 get '/' => sub {
     my $data = [ 'a' .. 'z'];
+
+    if ( params->{page} ) {
+        return halt('Attribut page must be an integer')
+            if params->{page} !~ /^\d+$/;
+    }
+
     template 'index', { 
         current_page => params->{'page'},
         entries_per_page => 5,
